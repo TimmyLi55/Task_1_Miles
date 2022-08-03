ad565610267f289bd7b8b75fdc875f4468687f37
TimmyLi <artelaw@gmail.com>
2022-08-03 10:19:09 +0600

    Задание 1. Мили

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int costTicket = 2540;
+        int rubForOneMiles = 20;
+        int bonusMiles = costTicket / rubForOneMiles;
+        System.out.println("Вам будет начислена " + bonusMiles + " бонусных(-ая) миля");
+
+    }
+}
